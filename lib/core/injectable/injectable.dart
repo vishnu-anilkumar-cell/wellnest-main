e1f69a353e08f0d61b5dfef8dd0cd02365abe185
Vishnu Anilkumar <vishnuanilkumar1010@gmail.com>
2025-04-30 12:57:56 +0530

    Delete lib directory

--- lib/core/injectable/injectable.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
-import 'package:wellnest/core/injectable/injectable.config.dart';
-
-GetIt getIt = GetIt.instance;
-@InjectableInit()
-Future<void> configureInjection(String env) async {
-  GetItInjectableX(getIt).init(environment: env);
-}
